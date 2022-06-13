778b0276bef3e08473989e35837f6bb3886e7bff
ifeoluwagodstime <ifeoluwagodstime2008@gmail.com>
2022-06-13 08:53:38 +0100

    commit

---
+++ js/statement/statement.js
@@ -0,0 +1,30 @@
+// let david = "girls"
+
+// if(david == "girls"){
+//     console.log("Mr. David how Many girlfriends do you have?")
+// }else{
+//     console.log("Mr. David borrow shakiru one of your girlfriends")
+// }
+
+// let now = new Date();
+// let day = now.getDay(); // Sunday - Saturday : 0 - 6
+
+// if(day == 5) {
+//     console.log("Have a nice weekend!");
+// } else {
+//     console.log("Have a nice day!");
+// }
+
+
+let gadget1 = "Iphone x"
+let gadget2 = "Iphone xr"
+
+if(gadget1 == gadget2){
+    console.log("You have the same gadget")
+}else if(gadget1 != gadget2){
+    console.log("you have different gadget")
+}else if(gadget1 === "iphone x"){
+    console.log("this two are not identical")
+}else{
+    console.log("Were don with the survey")
+}
